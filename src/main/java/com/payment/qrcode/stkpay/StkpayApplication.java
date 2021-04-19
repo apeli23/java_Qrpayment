7f65677633e2ca8a3b9fddc3c4770130540588ae
ryananyangu <ryananyangu@gmail.com>
2021-04-19 13:39:19 +0000

    initial commit

---
+++ src/main/java/com/payment/qrcode/stkpay/StkpayApplication.java
@@ -0,0 +1,13 @@
+package com.payment.qrcode.stkpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StkpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StkpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payment/qrcode/stkpay/StkpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.qrcode.stkpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StkpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
